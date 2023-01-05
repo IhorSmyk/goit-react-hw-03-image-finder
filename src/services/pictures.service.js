61e84731e2eeb9df3b68b40d8696e67af2151338
Ihor Smyk <ismyk256@gmail.com>
2023-01-05 19:01:24 +0200

    upd

--- src/services/pictures.service.js
+++ src/services/pictures.service.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
+// key = 32626693-37bc96ab6bf8bfcf28c619d39
+
+const picturesService = axios.create({
+  baseURL: 'https://pixabay.com/api/',
+  params: {
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: 12,
+    key: '32626693-37bc96ab6bf8bfcf28c619d39',
+  },
+});
+
 export const getPictures = async () => {
-const { data } = await axios.get(
-  'https://pixabay.com/api/?q=cat&page=1&key=32626693-37bc96ab6bf8bfcf28c619d39&image_type=photo&orientation=horizontal&per_page=12'
-    );
-    return data;
-}
+  const { data } = await picturesService.get('', {
+    params: { q: 'duck', page: 2 },
+  });
+  return data;
+};
